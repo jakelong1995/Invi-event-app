937e21d28eb89ac3b3685a98ad83df0c89304e7e
jakelong1995 <hoanglong2579@gmail.com>
2024-06-30 19:54:21 +0700

    Push to thinh

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,66 +1,45 @@
-import CategoryFilter from '@/components/shared/CategoryFilter';
-import Collection from '@/components/shared/Collection'
-import Search from '@/components/shared/Search';
-import { Button } from '@/components/ui/button'
-import { getAllEvents } from '@/lib/actions/event.actions';
-import { SearchParamProps } from '@/types';
-import Image from 'next/image'
-import Link from 'next/link'
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
+import Image from "next/image";
+import logopng from "@/public/assets/images/Invi.png";
 
 export default async function Home({ searchParams }: SearchParamProps) {
   const page = Number(searchParams?.page) || 1;
-  const searchText = (searchParams?.query as string) || '';
-  const category = (searchParams?.category as string) || '';
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
 
   const events = await getAllEvents({
     query: searchText,
     category,
     page,
-    limit: 6
-  })
+    limit: 6,
+  });
 
   return (
-    <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
-        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
-          <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
-            <Button size="lg" asChild className="button w-full sm:w-fit">
-              <Link href="#events">
-                Explore Now
-              </Link>
-            </Button>
-          </div>
+    <div className="flex flex-col md:flex-row gap-16 justify-between w-full items-center max-w-4xl sm:mx-auto m-4">
+      <div className="flex flex-col max-w-sm gap-8 items-center text-center md:items-start md:text-left">
+        <Image src={logopng} alt="logo" className="w-16" />
+        <h1 className="text-gray-800 font-semibold text-4xl md:text-5xl">
+          Delightful events <span className="text-rose-400">start here.</span>{" "}
+        </h1>
+        <p className="text-gray-600">
+          Set up an event page, invite friends and sell tickets. Host a
+          memorable event today.
+        </p>
+        <button className="px-6 py-2 bg-gray-800 rounded-lg text-white text-lg font-medium hover:bg-gray-600 border-none">
+          Create Your First Event
+        </button>
+      </div>
 
-          <Image 
-            src="/assets/images/hero.png"
-            alt="hero"
-            width={1000}
-            height={1000}
-            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
-          />
-        </div>
-      </section> 
-
-      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
-
-        <div className="flex w-full flex-col gap-5 md:flex-row">
-          <Search />
-          <CategoryFilter />
-        </div>
-
-        <Collection 
-          data={events?.data}
-          emptyTitle="No Events Found"
-          emptyStateSubtext="Come back later"
-          collectionType="All_Events"
-          limit={6}
-          page={page}
-          totalPages={events?.totalPages}
-        />
-      </section>
-    </>
-  )
+      <video
+        autoPlay
+        muted
+        loop
+        className="w-full mx-4 sm:mx-0 sm:w-1/2 bg-transparent"
+      >
+        <source src="https://cdn.lu.ma/landing/phone-dark.webm" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 }
