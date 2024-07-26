c3de0ea4b4f6ba55198b284932865b60841022d4
jakelong1995 <hoanglong2579@gmail.com>
2024-07-26 17:06:45 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['utfs.io'],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'utfs.io',
-        port: ''
-      }
-    ]
-  }
-}
-
-module.exports = nextConfig
+        protocol: "https",
+        hostname: "example.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "anotherdomain.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
