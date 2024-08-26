b308f7b0e7e45ea872e06066e1216cc03e7c1f0a
jakelong1995 <hoanglong2579@gmail.com>
2024-08-26 21:37:50 +0700

    restart

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,14 @@
-import Footer from "@/components/shared/Footer"
-import Header from "@/components/shared/Header"
+import Header from "@/components/ui/shared/Header";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-1">{children}</main>
-      <Footer />
     </div>
-  )
+  );
 }
---
+++ components/ui/shared/Header.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Header = () => {
+  return (
+    <header>
+      <Button>Button</Button>
+    </header>
+  );
+};
+
+export default Header;
